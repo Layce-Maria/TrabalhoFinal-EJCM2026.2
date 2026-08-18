421ddb37b7d3d7ee7bf676a0b71f4426df4d4553
daniel <danielrpiccoli@gmail.com>
2026-08-18 15:16:23 -0300

    feat: cria Authenticate.ts e implementa autenticação em rotas sensíveis

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -2,18 +2,21 @@
 import { Router } from "express";
 import { UserController } from "../controllers/UserController";
 import { ProductController } from "../controllers/ProductController";
+import { authenticateJWT } from "../middlewares/Authenticate";
 
 const router = Router();
 
 router.post("/users", UserController.create);
-router.get("/users/:id", UserController.getById);
-router.patch("/users/:id", UserController.update);
-router.delete("/users/:id", UserController.delete);
+
+router.get("/users/:id", authenticateJWT, UserController.getById);
+router.patch("/users/:id", authenticateJWT, UserController.update);
+router.delete("/users/:id", authenticateJWT, UserController.delete);
 
 router.post("/products", ProductController.create);
-router.get("/products/:id", ProductController.getById);
-router.patch("/products/:id", ProductController.update);
-router.delete("/products/:id", ProductController.delete);
+
+router.get("/products/:id", authenticateJWT, ProductController.getById);
+router.patch("/products/:id", authenticateJWT, ProductController.update);
+router.delete("/products/:id", authenticateJWT, ProductController.delete);
 
 
 export default router;
