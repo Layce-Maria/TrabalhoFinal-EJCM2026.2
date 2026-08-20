90edbb5324c08d66997123fa92ab580a1d5b9cfe
Layce-Maria <laycemaffonso@gmail.com>
2026-08-20 18:34:25 -0300

    feat:criaWishListControllerERotas

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -3,6 +3,11 @@ import { Router } from "express";
 import { UserController } from "../controllers/UserController";
 import { ProductController } from "../controllers/ProductController";
 import { VariantController } from "../controllers/VariantController";
+<<<<<<< HEAD
+=======
+import { authenticateJWT } from "../middlewares/Authenticate";
+import { WishListController } from "../controllers/WishListController";
+>>>>>>> cd192a6 (feat:criaWishListControllerERotas)
 
 const router = Router();
 
@@ -19,11 +24,17 @@ router.patch("/products/:id", ProductController.update);
 router.delete("/products/:id", ProductController.delete);
 
 //Product Variant Routes
-router.post("/productVariant",VariantController.create);
-router.get("/productVariant/:id",VariantController.getById);
-router.get("/productVariant/productId",VariantController.getVariantByProduct);
-router.patch("/productVariant/:id",VariantController.update);
-router.patch("/productVariant/:id",VariantController.updateStock);
-router.delete("/productVariant/:id",VariantController.delete);
+router.post("/productVariant",authenticateJWT, VariantController.create);
+router.get("/productVariant/:id",authenticateJWT, VariantController.getById);
+router.get("/productVariant/productId", authenticateJWT, VariantController.getVariantByProduct);
+router.patch("/productVariant/:id",authenticateJWT, VariantController.update);
+router.patch("/productVariant/:id",authenticateJWT, VariantController.updateStock);
+router.delete("/productVariant/:id",authenticateJWT, VariantController.delete);
+
+//Wishlist Routes
+router.post("/wishlist/:userId",authenticateJWT,  WishListController.create);
+router.get("/wishlist/:userId",authenticateJWT,  WishListController.get);
+router.delete("/wishlist/:userId/product/:productId", authenticateJWT, WishListController.deleteOne);
+router.delete("/wishlist/:userId",authenticateJWT, WishListController.deleteAll);
 
 export default router;
